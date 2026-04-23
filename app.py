94fbf6872cb1015a9c0088358e3339a82e0e0d60
Etido Ukpong <etido.ukpong@madetech.com>
2026-04-23 12:45:22 +0100

    change implementation

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+from controller import get_duties_from_db
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return []
+    duties = get_duties_from_db()
+    return render_template("index.html", duties=duties)
---
+++ controller.py
@@ -0,0 +1,8 @@
+# controller
+def get_duties_from_db():
+    # will turn data into duty objects to use in the app when it exists
+    pass
+
+def call_database():
+    # database doesn't exist yet
+    pass
