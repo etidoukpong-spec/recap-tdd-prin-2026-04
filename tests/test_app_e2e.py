e320861543d8109543d773bdd6d6dc1c9d54dfc2
Etido Ukpong <etido.ukpong@madetech.com>
2026-05-07 17:31:44 +0100

    test full flow for adding a duty

--- tests/test_app_e2e.py
+++ tests/test_app_e2e.py
@@ -9,7 +9,17 @@ test_app = app.test_client()
 def mock_request(page):
     def intercept_request(route):
         path = urlparse(route.request.url).path
-        response = test_app.get(path)
+        method = route.request.method
+
+        if method == "POST":
+            response = test_app.post(
+                path, 
+                data=route.request.post_data,
+                headers=dict(route.request.headers)
+                )
+
+        else:
+            response = test_app.get(path)
         route.fulfill(
             body=response.data,
             headers=dict(response.headers),
@@ -33,3 +43,16 @@ def test_heading_is_present_on_page(page: Page):
 def test_add_duty_button_is_present_on_page(page: Page):
     page.goto("http://127.0.0.1:5000")
     expect(page.get_by_role("button", name="Add Duty")).to_be_visible()
+
+def test_adding_a_new_duty_shows_it_on_the_page(page: Page):
+    page.goto("http://127.0.0.1:5000")
+
+    page.fill('input[name="identifier"]', "Duty 5")
+    page.fill('input[name="description"]', "Build and operate")
+    page.get_by_role("button", name="Add Duty").click()
+
+    print(page.content())
+
+    expect(page.get_by_role("heading", name="Duty 5")).to_be_visible()
+    expect(page.get_by_text("Build and operate")).to_be_visible()
+    expect(page.get_by_text("Duty 5 created!")).to_be_visible()
