69aa2ccd24bae60178a411c402de313cdd411d59
Etido Ukpong <etido.ukpong@madetech.com>
2026-05-07 05:53:36 +0100

    change returned object to pass test

--- duties/controller.py
+++ duties/controller.py
@@ -1,7 +1,7 @@
 from duties.duty import Duty
 
 def create_duty(identifier="", description=""):
-    return Duty("", "")
+    return Duty(identifier, description)
 
 def get_duties_from_db():
     # will turn data into duty objects to use in the app when it exists
