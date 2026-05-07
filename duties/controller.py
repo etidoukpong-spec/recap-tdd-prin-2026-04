1c0a7bf423053b44e85693b8d69ad56aa991adc3
Etido Ukpong <etido.ukpong@madetech.com>
2026-05-07 11:49:47 +0100

    add failing test for creating duty from form data

--- duties/controller.py
+++ duties/controller.py
@@ -1,6 +1,9 @@
 from duties import db
 from duties.duty import Duty
 
+def create_duty_from_form(form_data):
+    pass
+
 def get_duties_from_db():
     data = db.call_database()
     return [Duty(row["identifier"], row["description"]) for row in data]
--- tests/test_app_integration.py
+++ tests/test_app_integration.py
@@ -1,3 +1,5 @@
+from app import app
+
 from duties import controller
 from duties.duty import Duty
 
@@ -11,7 +13,7 @@ def test_db_is_called_successfully(mocker):
     mock_call.assert_called_once()
     assert isinstance(duties, list)
 
-def test_duties_are_returned(mocker):
+def test_duties_are_returned_from_db(mocker):
     mock_data = [{
         "identifier": "identifier", 
         "description": "description"
@@ -23,3 +25,18 @@ def test_duties_are_returned(mocker):
     assert len(duties) > 0
     assert isinstance(duties[0], Duty)
     assert duties[0].identifier == "identifier"
+
+def test_create_function_returns_a_duty_from_form_data(mocker):
+    mock_duty = Duty("identifier", "description")
+    mocker.patch("duties.duty.create_duty", return_value=mock_duty)
+
+    form_data = {
+        "identifier": "identifier",
+        "description": "description"
+    }
+
+    duty = controller.create_duty_from_form(form_data)
+
+    assert duty.identifier == form_data["identifier"]
+    assert duty.description == form_data["description"]
+
