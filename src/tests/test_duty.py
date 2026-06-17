e3111e5da08334a6fa1ead2ac7ab162b5b50d7ae
Etido Ukpong <etido.ukpong@madetech.com>
2026-06-17 12:50:42 +0100

    red:test duty object has identifier

--- src/tests/test_duty.py
+++ src/tests/test_duty.py
@@ -4,3 +4,7 @@ class Duty():
 def test_should_instantiate_duty_object():
     duty = Duty()
     assert duty is not None
+
+def test_should_have_an_identifier():
+    duty = Duty()
+    assert duty.id is not None
