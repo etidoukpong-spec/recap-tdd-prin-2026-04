e461785eb9cf0ee564c499bf0ed6ae0dcf2d9069
Etido Ukpong <etido.ukpong@madetech.com>
2026-06-24 09:33:01 +0100

    infra: update cleanup script to delete express service

--- terraform/cleanup.sh
+++ terraform/cleanup.sh
@@ -4,10 +4,18 @@ set -e
 SERVICE_EXISTS=$(aws ecs list-services --cluster etido-tdd-cluster-v2 --region eu-west-2 --query "serviceArns[?contains(@, 'etido-tdd-recap-363b')]" --output text 2>/dev/null)
 
 if [ -n "$SERVICE_EXISTS" ] && [ "$SERVICE_EXISTS" != "None" ]; then
-  echo "Found stale service. Forcing deletion..."
-  aws ecs delete-service --cluster etido-tdd-cluster-v2 --service etido-tdd-recap-363b --force --region eu-west-2
+    echo "Found stale service. Forcing deletion..."
+
+    aws ecs delete-express-gateway-service --cluster etido-tdd-cluster-v2 --service etido-tdd-recap-363b --force --region eu-west-2
+
+    echo "Waiting for Express service to turn INACTIVE..."
+
+    while aws ecs list-services --cluster etido-tdd-cluster-v2 --region eu-west-2 --query "serviceArns[?contains(@, 'etido-tdd-recap-363b')]" --output text | grep -q "etido-tdd-recap-363b"; do
+        echo "Still deleting... sleeping for 5 seconds..."
+        sleep 5
+    done
+        echo "Express service deleted successfully."
   
-  aws ecs wait services-inactive --cluster etido-tdd-cluster-v2 --services etido-tdd-recap-363b --region eu-west-2
 else
-  echo "No stale service detected. Proceeding safely."
+    echo "No stale service detected. Proceeding safely."
 fi
