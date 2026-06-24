16afaeddf0e47d67d392536da106f8b962978836
Etido Ukpong <etido.ukpong@madetech.com>
2026-06-24 09:14:46 +0100

    infra: add cleanup script dependency to the app repository

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,9 +1,17 @@
+resource "terraform_data" "clean_stale_service" {
+  provisioner "local-exec" {
+    when    = create
+    command = "${path.module}/cleanup.sh"
+  }
+}
+
 resource "aws_ecr_repository" "app_repo" {
   name                 = "etido-tdd-recap"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
+  depends_on = [terraform_data.clean_stale_service]
 }
 
 resource "aws_ecs_cluster" "app_cluster" {
