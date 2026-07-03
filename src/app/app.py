840afc430ff4002824cf5e4ddd2d15e4a1774894
Etido Ukpong <etido.ukpong@madetech.com>
2026-07-03 08:25:10 +0100

    fix: add error handling

--- src/app/app.py
+++ src/app/app.py
@@ -6,15 +6,19 @@ repository = DutyRepository()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    error_message = None
+    
     if request.method == "POST":
         id_chosen = request.form.get("duty_id")
         desc_typed = request.form.get("description")
 
-        duty = Duty(id=id_chosen,desc=desc_typed)
-
-        repository.add(duty)
+        try:
+            duty = Duty(id=id_chosen, desc=desc_typed)
+            repository.add(duty)
+        except ValueError as e:
+            error_message = str(e)
     
     options = Duty.get_options()
-    duties =  repository.read_all()
+    duties = repository.read_all()
     
-    return render_template("index.html", options=options, duties=duties)
+    return render_template("index.html", options=options, duties=duties, error_message=error_message)
