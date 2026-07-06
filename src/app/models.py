c8eacc9644596edaa7a86baf123fd2adbda3be58
Etido Ukpong <etido.ukpong@madetech.com>
2026-07-06 14:06:12 +0100

    set up initial data model

---
+++ src/app/models.py
@@ -0,0 +1,28 @@
+import uuid
+from peewee import *
+from config import DB_CONFIG
+
+db = PostgresqlDatabase(**DB_CONFIG)
+
+class BaseModel(Model):
+    """A base model that will use our Postgresql database"""
+    class Meta:
+        database = db
+
+class DutyModel(BaseModel): 
+    duty_id = UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
+    duty_name = CharField(unique=True)
+    duty_desc = CharField()
+
+    
+class CoinModel(BaseModel):
+    coin_id = UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
+    coin_name = CharField(unique=True)
+    is_complete = BooleanField()
+
+class Junction(BaseModel):
+    junction_id = UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
+    duty_id = ForeignKeyField(DutyModel, on_delete="CASCADE")
+    coin_id = ForeignKeyField(CoinModel, on_delete="CASCADE")
+
+db.create_tables([DutyModel, CoinModel, Junction])
