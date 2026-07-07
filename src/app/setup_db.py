8a74d42512fbfe911b9b73cf14c871b07fa2ad16
Etido Ukpong <etido.ukpong@madetech.com>
2026-07-07 16:59:26 +0100

    seed duties

--- src/app/setup_db.py
+++ src/app/setup_db.py
@@ -1,10 +1,20 @@
+import json
+
 from src.app.models import db, Coin, Duty, Junction
 
 def init_db():
+    with open('src/app/seed_data.json') as json_data:
+        seed_data = json.load(json_data)
+        all_duties = seed_data['duties']
     db.connect()
     db.create_tables([Coin, Duty, Junction], safe=True)
     
     print("Tables created successfully in the 'coins' schema!")
+
+    Duty.insert_many(all_duties).execute()
+
+    print("Duties seeded successfully in the 'duties' table!")
+    
     
     db.close()
 
